c12aa5dcb10aa307a11979b37f655046193a806e
JuliaHusar <juliahusar03@gmail.com>
2025-02-15 16:03:03 -0500

    updated CORS

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 
-
-app.use('/', express.static('public'));
+app.use(cors());
 
 const budget = JSON.parse(fs.readFileSync('data-json.json', 'utf8'));
-
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
-})
 
 app.get('/budget', (req, res) => {
     res.json(budget);
